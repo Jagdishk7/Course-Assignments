8fe85f99d0bda3b3b2acd9e3c0783433901a2915
jagdeesh kushwaha <pwdacait7@gmail.com>
2023-03-14 01:23:05 +0530

    javascript added to 6th assignment

---
+++ Javascript/Milestone 1/JavaScript Foundation/6 Vowel Counter/script.js
@@ -0,0 +1,13 @@
+function countBtn(){
+    const input = document.getElementById('inputBox').value;
+    const output = document.getElementById('outputBox');
+
+    let count = 0;
+    for (let i = 0; i < input.length; i++) {
+        const element = input[i];
+        if (element == 'a' || element == 'e' || element == 'i' || element == 'o' || element == 'u') {
+            count += 1;
+        }
+    }
+    console.log(count)
+}
