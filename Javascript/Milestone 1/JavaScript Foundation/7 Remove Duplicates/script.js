bea58f2800c90951c4ce99bf7dbeca1f159f11d6
jagdeesh kushwaha <pwdacait7@gmail.com>
2023-03-15 00:32:17 +0530

    7th Assignment Completed

--- Javascript/Milestone 1/JavaScript Foundation/7 Remove Duplicates/script.js
+++ Javascript/Milestone 1/JavaScript Foundation/7 Remove Duplicates/script.js
@@ -1,4 +1,9 @@
-const cart =['mango','orange','mango', 'lichi']
+var cart =['mango','orange','mango', 'lichi']
+
 function removeDuplicates(cart){
-    
+    return cart.filter((item,index)=> {
+        return cart.indexOf(item) === index 
+    })
 }
+
+console.log(removeDuplicates(cart))
