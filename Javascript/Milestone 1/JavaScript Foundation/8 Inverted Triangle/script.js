e12a92dc1c0ae51bf845113b8ceb44f88de516af
jagdeesh kushwaha <pwdacait7@gmail.com>
2023-03-15 23:39:20 +0530

    Completed 8th assignment

---
+++ Javascript/Milestone 1/JavaScript Foundation/8 Inverted Triangle/script.js
@@ -0,0 +1,14 @@
+let i = prompt('Enter Row Length');
+let star ='';
+// External loop
+for (let a = i; a >= 1; a--) {
+  
+  
+  // printing star
+  for (let k = 0; k < i; k++) {
+    star += "*";
+  }
+  star += "\n";
+  i--;
+}
+console.log(star);
